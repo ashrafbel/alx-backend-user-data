50b9b37db6c22a5c711b554367c1a0b2fa973aff
ashrafbel <ashrafmus2@gmail.com>
2024-11-07 15:20:04 -0800

    record

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -16,19 +16,17 @@ def filter_datum(fields: List[str], redaction: str, message: str,
 
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
-        """
+    """
+
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
-        "Initialize"
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        "Format log record and redact specified fields as required."
-        m = record.getMessage()
-        m = message = filter_datum(self.fields, self.REDACTION, m, ";")
-        record.m = m
-        return super().format(record)
+        """ Returns filtered values from log records """
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
