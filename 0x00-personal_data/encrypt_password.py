a3f83a36abebd559382039ce698f7311300b47d4
ashrafbel <ashrafmus2@gmail.com>
2024-11-08 01:36:46 +0000

    pass

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,3 +10,8 @@ def hash_password(password: str) -> bytes:
     passWordBytes = password.encode('utf-8')
     hashSalted = bcrypt.hashpw(passWordBytes, bcrypt.gensalt())
     return hashSalted
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Check if the provided password matches the hashed password"""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
