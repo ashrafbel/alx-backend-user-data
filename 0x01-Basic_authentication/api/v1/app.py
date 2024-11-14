491ca2c1db6800494512c470e7827228f4ebe589
ashrafbel <ashrafmus2@gmail.com>
2024-11-14 23:29:13 +0000

    Request validation

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,6 +12,11 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+AUTH_TYPE = os.getenv("AUTH_TYPE")
+if AUTH_TYPE == "auth":
+    from api.v1.auth.auth import Auth
+    auth = Auth()
 
 
 @app.errorhandler(404)
@@ -31,6 +36,25 @@ def unauthorized(error):
 def forbidden(error):
     "handler for 403 Forbidden"
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request():
+    "Verifies a user's identity before handling a request"
+    if auth is None:
+        return None
+    excluded_paths = ['/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/',
+        ]
+    if request.path in excluded_paths:
+        return None
+    if not auth.require_auth(request.path, excluded_paths):
+        return None
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
 
 
 if __name__ == "__main__":
--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -23,7 +23,9 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """ Method to retrieve the authorization header. """
-        return None
+        if request is None or 'Authorization' not in request.headers:
+            return None
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         "Method to extract user from the request."
