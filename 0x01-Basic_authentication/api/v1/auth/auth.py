61e5cef329501eb238d0c7df24f8c8e88c1c3717
ashrafbel <ashrafmus2@gmail.com>
2024-11-14 21:14:39 +0000

    authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,7 +9,17 @@ class Auth:
     "Auth class"
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         "A method to check if authentication is required."
-        return False
+        if path is None:
+            return True
+        if not excluded_paths:
+            return True
+        normalized_path = path.rstrip("/")
+        excluded_paths_normalized = [
+            excluded_path.rstrip("/") for excluded_path in excluded_paths
+        ]
+        if normalized_path in excluded_paths_normalized:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ Method to retrieve the authorization header. """
